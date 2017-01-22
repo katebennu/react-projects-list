0e0af1831478791becda9080252ff41cf429baca
katebennu <kate.dorrer@gmail.com>
2017-01-22 21:23:31 +0200

    text upd

--- apps/weatherapp/js/index.js
+++ apps/weatherapp/js/index.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
                 var text = "<h2>" + json.weather[0].main + "</h2>";
                 var t = (json.main.temp - 273.15).toFixed(1);
                 var temp = "<h1>" + t + "&deg; C</h1>";
-                var img = "<img src='http://openweathermap.org/img/w/"+json.weather[0].icon + ".png'>"
+                var img = "<img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>"
                 $("#weather").html(name + temp + text + img);
             });
         });
