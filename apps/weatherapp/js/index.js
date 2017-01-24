02bbc96415ae241207b4db2a7baf50fc9f1d12dd
katebennu <kate.dorrer@gmail.com>
2017-01-25 00:40:35 +0200

    changed paths to staticfiles, minor css styling

--- apps/weatherapp/js/index.js
+++ apps/weatherapp/js/index.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
         $.getJSON(url, function (json) {
             var name = "<h4>" + json.name + "</h4>";
             var text = "<h2>" + json.weather[0].main + "</h2>";
+
             var t = (json.main.temp - 273.15).toFixed(1);
             var temp = "<h1>" + t + "&deg; C</h1>";
             var img = "<img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>"
