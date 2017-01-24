41b0ded5495ce9fe6fbbfd037d2aab4cd1a405e7
katebennu <kate.dorrer@gmail.com>
2017-01-24 14:01:37 +0200

    upd

--- apps/weatherapp/js/index.js
+++ apps/weatherapp/js/index.js
@@ -1,20 +1,34 @@
-$(document).ready(function() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
+$(document).ready(function () {
 
-            var lat = (position.coords.latitude).toFixed(0),
-                lon = (position.coords.longitude).toFixed(0);
-            //$("#test").html("latitude: " + lat + "<br>longitude: " + lon);
-            var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=87703abe8c78fd2ec0117e25f494fa76";
+    function success(position) {
 
-            $.getJSON(url, function(json) {
-                var name = "<h4>" + json.name + "</h4>";
-                var text = "<h2>" + json.weather[0].main + "</h2>";
-                var t = (json.main.temp - 273.15).toFixed(1);
-                var temp = "<h1>" + t + "&deg; C</h1>";
-                var img = "<img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>"
-                $("#weather").html(name + temp + text + img);
-            });
+        var lat = (position.coords.latitude).toFixed(5);
+        var lon = (position.coords.longitude).toFixed(5);
+        console.log(lat, lon);
+
+        var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=87703abe8c78fd2ec0117e25f494fa76";
+        console.log(url);
+
+        $.getJSON(url, function (json) {
+            var name = "<h4>" + json.name + "</h4>";
+            var text = "<h2>" + json.weather[0].main + "</h2>";
+            var t = (json.main.temp - 273.15).toFixed(1);
+            var temp = "<h1>" + t + "&deg; C</h1>";
+            var img = "<img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>"
+            $("#weather").html(name + temp + text + img);
         });
+
     }
+
+    function error (err) {
+        console.warn(err.message);
+        var url = "http://api.openweathermap.org/data/2.5/weather?q=helsinki&appid=87703abe8c78fd2ec0117e25f494fa76";
+        console.log(url);
+    }
+
+    
+
+    navigator.geolocation.getCurrentPosition(success, error);
 });
+
+
