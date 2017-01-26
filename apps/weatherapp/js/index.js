0cf6a21fec60cae4fe2fef0b4580db269254e12d
katebennu <kate.dorrer@gmail.com>
2017-01-26 04:30:14 +0200

    resize for mobile

--- apps/weatherapp/js/index.js
+++ apps/weatherapp/js/index.js
@@ -11,11 +11,13 @@ $(document).ready(function () {
         var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=87703abe8c78fd2ec0117e25f494fa76";
         //console.log(url);
 
+
+
         $.getJSON(url, function (json) {
             var name = "<h4>" + json.name + "</h4>";
             var text = "<h2>" + json.weather[0].main + "</h2>";
-
             var t = (json.main.temp - 273.15).toFixed(0);
+            // if (document.getElementById("option1").checked == false) t = t * 1.8 + 32;
             var temp = "<h1>" + t + "&deg;C</h1>";
             var img = "<img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>"
             $("#city").html(name);
@@ -33,6 +35,7 @@ $(document).ready(function () {
         $.getJSON(url, function (json) {
             var name = "<h4>" + json.name + "</h4>";
             var text = "<h2>" + json.weather[0].main + "</h2>";
+            // if (document.getElementById("option1").checked == false) t = t * 1.8 + 32;
             var t = (json.main.temp - 273.15).toFixed(0);
             var temp = "<h1>" + t + "&deg;C</h1>";
             var img = "<img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>"
