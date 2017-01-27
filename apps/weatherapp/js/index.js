a71c2381833c2f034d052cbdd83f1913dcf5cc48
katebennu <kate.dorrer@gmail.com>
2017-01-27 16:23:23 +0200

    DRYing weather app

--- apps/weatherapp/js/index.js
+++ apps/weatherapp/js/index.js
@@ -1,53 +1,34 @@
-$(document).ready(function () {
+function output(url) {
+    $.getJSON(url, function(json) {
+        var name = "<h4>" + json.name + "</h4>";
+        var text = "<h2>" + json.weather[0].main + "</h2>";
+        var t = (json.main.temp - 273.15).toFixed(0);
+        // if (document.getElementById("option1").checked == false) t = t * 1.8 + 32;
+        var temp = "<h1>" + t + "&deg;C</h1>";
+        var img = "<img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>"
+        $("#city").html(name);
+        $("#temp").html(temp);
+        $("#text").html(text);
+        $("#img").html(img);
+    });
+}
 
-    var options = { enableHighAccuracy: true, maximumAge: 100, timeout: 60000 };
+var options = {
+    enableHighAccuracy: true,
+    maximumAge: 100,
+    timeout: 60000
+};
 
-    function success(position) {
+function success(position) {
+    var lat = (position.coords.latitude).toFixed(10);
+    var lon = (position.coords.longitude).toFixed(10);
+    var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=87703abe8c78fd2ec0117e25f494fa76";
+    output(url);
+}
 
-        var lat = (position.coords.latitude).toFixed(10);
-        var lon = (position.coords.longitude).toFixed(10);
-        //console.log(lat, lon);
+function error(err) {
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=helsinki&appid=87703abe8c78fd2ec0117e25f494fa76";
+    output(url);
+}
 
-        var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=87703abe8c78fd2ec0117e25f494fa76";
-        //console.log(url);
-
-
-
-        $.getJSON(url, function (json) {
-            var name = "<h4>" + json.name + "</h4>";
-            var text = "<h2>" + json.weather[0].main + "</h2>";
-            var t = (json.main.temp - 273.15).toFixed(0);
-            // if (document.getElementById("option1").checked == false) t = t * 1.8 + 32;
-            var temp = "<h1>" + t + "&deg;C</h1>";
-            var img = "<img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>"
-            $("#city").html(name);
-            $("#temp").html(temp);
-            $("#text").html(text);
-            $("#img").html(img);
-        });
-
-    }
-
-    function error (err) {
-        //console.warn(err.message);
-        var url = "http://api.openweathermap.org/data/2.5/weather?q=helsinki&appid=87703abe8c78fd2ec0117e25f494fa76";
-        //console.log(url);
-        $.getJSON(url, function (json) {
-            var name = "<h4>" + json.name + "</h4>";
-            var text = "<h2>" + json.weather[0].main + "</h2>";
-            // if (document.getElementById("option1").checked == false) t = t * 1.8 + 32;
-            var t = (json.main.temp - 273.15).toFixed(0);
-            var temp = "<h1>" + t + "&deg;C</h1>";
-            var img = "<img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>"
-            $("#city").html(name);
-            $("#temp").html(temp);
-            $("#text").html(text);
-            $("#img").html(img);
-        });
-    }
-
-    navigator.geolocation.getCurrentPosition(success, error, options);
-
-});
-
-
+navigator.geolocation.getCurrentPosition(success, error, options);
