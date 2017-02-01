0cb4a25cb47815c9f3151fd2ab43370c12eaa79f
katebennu <kate.dorrer@gmail.com>
2017-02-01 02:16:18 +0200

    upper soc buttons

--- apps/wikiapp/js/index.js
+++ apps/wikiapp/js/index.js
@@ -1,3 +1,11 @@
+/*
+/!*
+ XMLHttpRequest cannot load https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=keyword&prop=info&inprop=url&utf8=&format=json.
+ No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://localhost:63342' is therefore not allowed access.
+
+ same on kbennu.com
+ *!/
+
 
 var responseData = [];
 
@@ -11,9 +19,39 @@ function searchQuery() {
             console.log(this.status);
         }
     };
-    xhttp.open('GET', 'https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=pageimages%7Cpageterms&titles=Albert%20Einstein', true);
-    xhttp.setRequestHeader('User-Agent', 'wikiapp/0.1 (http://kbennu.com/apps/wikiapp/)'); //setRequestHeader(header, value)
+    xhttp.open('GET', 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=keyword&prop=info&inprop=url&utf8=&format=json', true);
+    //xhttp.setRequestHeader('Api-User-Agent', 'wikiapp/0.1 (http://kbennu.com/apps/wikiapp/)'); //setRequestHeader(header, value)
     xhttp.send();
 }
 
 document.getElementById('searchBtn').addEventListener('click', searchQuery);
+
+//https://en.wikipedia.org/w/api.php?action=query&origin=https://en.wikipedia.org&meta=tokens&type=csrf&format=json&formatversion=2&prop=pageimages%7Cpageterms&titles=Albert%20Einstein
+// ?action=query&meta=tokens&type=csrf&format=json&formatversion=2&prop=pageimages%7Cpageterms&titles=Albert%20Einstein
+//&meta=tokens&type=csrf
+
+//https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + keyword + "&prop=info&inprop=url&utf8=&format=json
+
+
+*/
+
+
+function ajax (keyword) { //AJAX request
+    $.ajax({
+        url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + keyword + "&prop=info&inprop=url&utf8=&format=json",
+        dataType: "jsonp",
+        success: function(response) {
+            console.log(response.query);
+            if (response.query.searchinfo.totalhits === 0) {
+                showError(keyword);
+            }
+
+            else {
+                showResults(response);
+            }
+        },
+        error: function () {
+            alert("Error retrieving search results, please refresh the page");
+        }
+    });
+}
