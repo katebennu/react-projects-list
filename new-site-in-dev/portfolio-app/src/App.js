796ec8d553859ad9bfed45130b99aa54ddc7c16c
katebennu <kate.dorrer@gmail.com>
2017-04-22 15:43:01 +0300

    pass down data between components

--- new-site-in-dev/portfolio-app/src/App.js
+++ new-site-in-dev/portfolio-app/src/App.js
@@ -4,7 +4,7 @@ class Tile extends React.Component {
     render() {
         return (
             <div className="tile">
-                <p>Hi, I'm a Tile </p>
+                <p>Hi, I'm a {this.props.name} </p>
             </div>
         );
     }
@@ -13,7 +13,7 @@ class Tile extends React.Component {
 
 class Panel extends React.Component {
     render() {
-        const data = [
+        const items = [
             {'name': 'First item', 'text': 'This is the text of the first item'},
             {'name': 'Second item', 'text': 'This is the text of the second item'},
             {'name': 'Third item', 'text': 'This is the text of the third item'},
@@ -22,9 +22,9 @@ class Panel extends React.Component {
             {'name': 'Third item', 'text': 'This is the text of the third item'},
         ];
 
-        const tiles = data.map((data, i) => {
+        const tiles = items.map((items, i) => {
             return (
-                <Tile key={i}/>
+                <Tile key={i} name={items['name']}/>
             )
         });
 
@@ -36,13 +36,32 @@ class Panel extends React.Component {
     }
 }
 
+class Info extends React.Component {
+    render() {
+        const data = {
+            'name': 'Kay Bennu',
+            'title': 'Web Developer',
+            'description': 'Junior web developer with business background. Currently open for new projects, job and internship opportunities',
+            'links': [
+                {'name': 'Github', 'link': 'https://github.com/katebennu'},
+                {'name': 'Linkedin', 'link': 'https://www.linkedin.com/in/ekaterinadorrer/'},
+                {'name': 'Codewars', 'link': 'https://www.codewars.com/users/katebennu'},
+                {'name': 'Codepen', 'link': 'http://codepen.io/KateBennu/'}
+            ]
+        };
+        return <div>{JSON.stringify(data)}</div>;
+    }
+}
+
 class Page extends React.Component {
 
 
     render() {
         return (
             <div className="container page">
+
                 <div className="page-content">
+                    <Info/>
                     <Panel/>
                 </div>
             </div>
